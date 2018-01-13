defd7bc33ac5dd08db1b6821f24bb1c07cde09f4
SteelPh0enix <wojciech_olech@hotmail.com>
2018-01-13 23:41:38 +0100

    Input, now featuring sys.argv[1]!

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
 """Entry point"""
 import json
+import sys
 from ytcutter import YTCutter
 
 if __name__ == '__main__':
-    data = json.load(open('input.json'))
+    if len(sys.argv) < 2:
+        print("USAGE: python . [JSON file with input data")
+        sys.exit(1)
+
+    with open(sys.argv[1]) as f:
+        data = json.load(f)
 
     YTCutter().run(data)
